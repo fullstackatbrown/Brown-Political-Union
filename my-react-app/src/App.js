7d6d9c827068ad334e2982adff259d7552e8290d
NaphatPRM <autnaphat@gmail.com>
2022-11-23 18:26:18 -0500

    Add frontend initial

---
+++ my-react-app/src/App.js
@@ -0,0 +1,360 @@
+import './App.css';
+
+function App() {
+  return <MacBookAir1 {...macBookAir1Data} />;
+}
+
+export default App;
+
+function MacBookAir1(props){
+  const {
+    hostedVirtualTourPhotoOption1,
+    spanText1,
+    spanText2,
+    spanText3,
+    spanText4,
+    spanText5,
+    spanText6,
+    spanText7,
+    spanText8,
+    spanText9,
+    spanText10,
+    spanText11,
+    spanText12,
+    spanText13,
+    spanText14,
+    spanText141,
+    spanText15,
+    spanText16,
+    spanText17,
+    spanText18,
+    spanText19,
+    spanText20,
+    spanText21,
+    spanText22,
+    spanText23,
+    spanText24,
+    spanText25,
+    spanText26,
+    spanText27,
+    spanText28,
+    spanText29,
+    spanText30,
+    spanText31,
+    spanText32,
+    spanText33,
+    freeMail,
+    x10770,
+    x360
+  } = props;
+
+  return (
+      <div className="container-center-horizontal">
+        <div className="macbook-air-1 screen">
+          <div className="frame-1">
+            <div className="flex-col">
+              <div className="flex-col-1">
+                <div className="overlap-group2">
+                  <img className="hosted-virtual-tour-photo-option-1" src={require('./hosted-virtual-tour-photo-option-1-1.jpeg')} alt="Hosted Virtual Tour"/>
+                  <div className="rectangle-1"></div>
+                  <h1 className="title inter-bold-white-48px">
+                    <span className="inter-bold-white-48px">
+                      {spanText1}
+                    </span>
+                  </h1>
+                  <div className="about-us inter-bold-white-20px">
+                    <span className="inter-bold-white-20px">
+                      {spanText2}
+                    </span>
+                  </div>
+                  <div className="events inter-bold-white-20px">
+                    <span className="inter-bold-white-20px">
+                      {spanText3}
+                    </span>
+                  </div>
+                  <div className="constitution inter-bold-white-20px">
+                    <span className="inter-bold-white-20px">
+                      {spanText4}
+                    </span>
+                  </div>
+                  <div className="parties inter-bold-white-20px">
+                    <span className="inter-bold-white-20px">
+                      {spanText5}
+                    </span>
+                  </div>
+                  <div className="leadership inter-bold-white-20px">
+                    <span className="inter-bold-white-20px">
+                      {spanText6}
+                    </span>
+                  </div>
+                  <div className="place inter-bold-white-20px">
+                    <span className="inter-bold-white-20px">
+                      {spanText7}
+                    </span>
+                  </div>
+                  <div className="rectangle-2"></div>
+                  <p className="fostering-ideologica inter-bold-black-36px">
+                    <span className="inter-bold-white-36px">
+                      {spanText8}
+                    </span>
+                  </p>
+                  <div className="block-blank"></div>
+                  <div className="about-us-1 inter-bold-black-30px">
+                    <span className="inter-bold-black-30px">
+                      {spanText9}
+                    </span>
+                  </div>
+                  <p className="lids inter-normal-black-18px">
+                    <span className="inter-normal-black-18px">
+                      {spanText10}
+                    </span>
+                  </p>
+                  <div className="events-1 inter-bold-black-30px">
+                    <span className="inter-bold-black-30px">
+                      {spanText11}
+                    </span>
+                  </div>
+                </div>
+                <div className="block-blank-2"></div>
+                <div className="flex-row">
+                  <div className="rectangle-4">
+
+                  </div>
+                  <div className="flex-col-2">
+                    <div className="in-person inter-normal-black-20px">
+                      <span className="inter-normal-black-20px">
+                        {spanText12}
+                      </span>
+                    </div>
+                    <p className="social-justice inter-normal-black-30px">
+                      <span className="inter-normal-black-30px">
+                        {spanText13}
+                      </span>
+                    </p>
+                    <p className="believe inter-normal-black-18px">
+                      <span className="inter-normal-black-18px">
+                        {spanText14}
+                      </span>
+                    </p>
+                    <p className="believe-date inter-normal-black-18px">
+                      <span className="inter-normal-black-18px">
+                        {spanText141}
+                      </span>
+                    </p>
+                    <div className="overlap-group1">
+                      <div className="place-1 inter-bold-white-14px">
+                        <span className="inter-bold-white-14px">
+                          {spanText15}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex-col-3">
+                  <div className="constitution-1 inter-bold-black-30px">
+                    <span className="inter-bold-black-30px">
+                      {spanText16}
+                    </span>
+                  </div>
+                  <p className="lids-1 inter-normal-black-18px">
+                    <span className="inter-normal-black-18px">
+                      {spanText17}
+                    </span>
+                  </p>
+                  <div className="overlap-group">
+                    <div className="link-to-full inter-bold-white-14px">
+                      <span className="inter-bold-white-14px">
+                        {spanText18}
+                      </span>
+                    </div>
+                  </div>
+                    <div className="parties-1 inter-bold-black-30px">
+                      <span className="inter-bold-black-30px">
+                        {spanText19}
+                      </span>
+                    </div>
+                    <p className="lids-2 inter-normal-black-18px">
+                      <span className="inter-normal-black-18px">
+                        {spanText20}
+                      </span>
+                    </p>
+                    <div className="leadership-1 inter-bold-black-30px">
+                      <span className="inter-bold-black-30px">
+                        {spanText21}
+                      </span>
+                    </div>
+                    <div className="bio-container">
+                      <Bio1 />
+                      <Bio1 />
+                      <Bio1 />
+                    </div>
+                    <div className="bio-container">
+                      <Bio1 />
+                      <Bio1 />
+                      <Bio1 />
+                    </div>
+                    <div className="place-2 inter-bold-black-30px">
+                      <span className="inter-bold-black-30px">
+                        {spanText22}
+                      </span>
+                    </div>
+                </div>
+                <div className="flex-row-1">
+                  <div className="flex-col-4">
+                    <img className="free-mail-icon" src={freeMail} alt="free-mail-icon" />
+                    <div className="bpubrown inter-normal-black-20px">
+                      <span className="inter-normal-black-20px">
+                        {spanText23}
+                      </span>
+                    </div>
+                  </div>
+                  <div className="flex-col-5">
+                    <img className="icon-instagram" src={x10770} alt="instagram" />
+                    <div className="brownpoliticalunion inter-normal-black-20px">
+                      <span className="inter-normal-black-20px">
+                        {spanText24}
+                      </span>
+                    </div>
+                  </div>
+                  <div className="flex-col-6">
+                    <img className="random-x" src={x360} alt="SthSth" />
+                    <div className="brownpoliticalunion-1 inter-normal-black-20px">
+                      <span className="inter-normal-black-20px">
+                        {spanText25}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div className="overlap-group3">
+                  <div className="flex-col-7">
+                    <div className="bpu-logo inter-bold-white-48px">
+                      <span className="inter-bold-white-48px">
+                        {spanText26}
+                      </span>
+                    </div>
+                    <p className="lids-3 inter-normal-white-18px">
+                      <span className="inter-normal-white-18px">
+                        {spanText27}
+                      </span>
+                    </p>
+                  </div>
+                  <div className="group-3 inter-bold-white-20px">
+                    <div className="flex-col-8">
+                      <div className="about-us-2">
+                        <span className="inter-bold-white-20px">
+                          {spanText28}
+                        </span>
+                      </div>
+                      <div className="parties-2">
+                        <span className="inter-bold-white-20px">
+                          {spanText29}
+                        </span>
+                      </div>
+                      <div className="constitution-2">
+                        <span className="inter-bold-white-20px">
+                          {spanText30}
+                        </span>
+                      </div>
+                    </div>
+                    <div className="flex-col-9">
+                      <div className="events-2">
+                        <span className="inter-bold-white-20px">
+                          {spanText31}
+                        </span>
+                      </div>
+                      <div className="leadership-2">
+                        <span className="inter-bold-white-20px">
+                          {spanText32}
+                        </span>
+                      </div>
+                      <div className="place-3">
+                        <span className="inter-bold-white-20px">
+                          {spanText33}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  );
+}
+
+function Bio1(props) {
+  const { classname } = props;
+
+  return (
+      <div className={'bio1-3 ${classname || ""}'}>
+        <div className="eclipse-1">
+
+        </div>
+        <div className="place-4 inter-bold-black-30px">
+          <span className="span-1 inter-bold-black-30px">
+            Name
+          </span>
+        </div>
+        <div className="role inter-normal-black-24px">
+          <span className="span-1 inter-normal-black-24px">
+            Role
+          </span>
+        </div>
+        <p className="bio-lid inter-normal-black-18px">
+          <span className="span-1 inter-normal-black-18px">
+            Bio : Lorem Ipsum dolor sit amel, consectatur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+          </span>
+        </p>
+      </div>
+  );
+}
+
+// const bio15Data = {
+//   className: "bio1-1"
+// }
+//
+// const bio16Data = {
+//   className: "bio1-2"
+// }
+
+const macBookAir1Data = {
+  hostedVirtualTourPhotoOption1 : "'./hosted-virtual-tour-photo-option-1-1.jpeg'",
+  spanText1 : "BPU(Logo)",
+  spanText2 : "About us",
+  spanText3 : "Events",
+  spanText4 : "Constitution",
+  spanText5 : "Parties",
+  spanText6 : "Leadership",
+  spanText7 : "Contact",
+  spanText8 : "Fostering ideological discourse, challenging perspectives, effecting personal-political growth",
+  spanText9 : "About Us",
+  spanText10 : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  spanText11 : "Events",
+  spanText12 : "In person",
+  spanText13 : "Social Justice and Free Speech on Campus with Peter Boghossian",
+  spanText14 : "Do you believe Social Justice has a place in the classroom? Do you think it stifles speech and progress? Or are you somewhere in-between? Philosopher Peter Boghossian will help us critically examine the role of Social Justice in education",
+  spanText141 : "Wednesday, Nov 4th @7PM. Main Green",
+  spanText15 : "Register",
+  spanText16 : "Constitution",
+  spanText17 : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  spanText18 : "Link to full text",
+  spanText19 : "Parties",
+  spanText20 : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  spanText21 : "Leadership",
+  spanText22 : "Contact",
+  freeMailIcon142Thumb1 : "free-mail-icon-142-thumb-1-1.png",
+  spanText23 : "BPU@brown.edu",
+  x10770421 : "1077042-1-1.png",
+  spanText24 : "@/brownpoliticalunion",
+  x360_F_321877261_22Blejlx609Sadv9Vf : "360-f-321877261-22blejlx609sadv9vfphdxhxsl5jnfng-1-1.png",
+  spanText25 : "@/brownpoliticalunion",
+  spanText26 : "BPU (Logo)",
+  spanText27 : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
+  spanText28 : "About us",
+  spanText29 : "Parties",
+  spanText30 : "Constitution",
+  spanText31 : "Events",
+  spanText32 : "Leadership",
+  spanText33 : "Contact",
+}
