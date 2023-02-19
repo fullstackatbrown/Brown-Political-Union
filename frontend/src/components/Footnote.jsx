7465acdc2dd686898181a6943e123e2e73a18d51
Naphat Permpredanun <68365175+NaphatPRM@users.noreply.github.com>
2023-02-19 00:03:58 -0500

    Add files via upload

---
+++ frontend/src/components/Footnote.jsx
@@ -0,0 +1,47 @@
+const Footnote = () => {
+    return (
+        <section className="h-64 bg-custom_brown">
+            <div className="float-left inline-block pl-10 pt-12">
+                <div className="text-5xl text-white font-bold">
+                    BPU
+                </div>
+                <div className="h-5">
+
+                </div>
+                <div className="text-l text-white">
+                    The Brown Political Union is a nonpartisan student-run organization
+                </div>
+                <div className="text-l text-white">
+                    dedicated to providing a forum for respectful political.
+                </div>
+                <div className="text-l text-white">
+                    discourse on campus.
+                </div>
+            </div>
+
+            <div className="float-right grid grid-rows-2 grid-flow-col gap-10 pt-12 pr-20">
+                <div className="text-2xl text-white font-bold inline-block">
+                    About Us
+                </div>
+                <div className="text-2xl text-white font-bold inline-block">
+                    Events
+                </div>
+                <div className="text-2xl text-white font-bold inline-block">
+                    Constitution
+                </div>
+                <div className="text-2xl text-white font-bold inline-block">
+                    Parties
+                </div>
+                <div className="text-2xl text-white font-bold inline-block">
+                    Leadership
+                </div>
+                <div className="text-2xl text-white font-bold inline-block">
+                    Contact
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Footnote;
+
