77de55a120514407cd5618ed397b2fea5ab82711
Naphat Permpredanun <68365175+NaphatPRM@users.noreply.github.com>
2023-02-19 00:04:33 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
-import "./App.css";
 import Navbar from "./components/Navbar";
+import Footnote from "./components/Footnote";
+import './App.css';
 
 function App() {
   return (
-    <Navbar></Navbar>
-  )
+    <div>
+      <Navbar></Navbar>
+        <div className="h-52">
+            EVENTS
+        </div>
+        <div className="h-52">
+            EVENTS 2
+        </div>
+      <Footnote></Footnote>
+    </div>
+  );
 }
 
 export default App;
