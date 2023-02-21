4ccd34fcd7906fa8f92c2d97b4d68cea29b342f2
Naphat Permpredanun <68365175+NaphatPRM@users.noreply.github.com>
2023-02-20 21:17:13 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div>
       <Navbar></Navbar>
-        <div className="h-52">
-            EVENTS
-        </div>
-        <div className="h-52">
-            EVENTS 2
-        </div>
-      <Footnote></Footnote>
-    </div>
   );
 }
 
