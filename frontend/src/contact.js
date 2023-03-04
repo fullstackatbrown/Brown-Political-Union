30d8af0e4a696f78ff0db0f33113ddae82a8fda1
Naphat Permpredanun <68365175+NaphatPRM@users.noreply.github.com>
2023-03-04 11:22:38 -0500

    Add files via upload

---
+++ frontend/src/contact.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function Contact() {
+    return (
+        <div>
+            <div className="h-48 mt-32 justify-center">
+                <div className="text-center text-3xl text-black font-bold">
+                    Contacts
+                </div>
+            </div>
+            <div className="h-32"> </div>
+        </div>
+    );
+}
+
+export default Contact;
---
+++ frontend/src/event.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Bio from "./components/Bio";
+
+function Events() {
+    return (
+        <div>
+            <div className="h-48 mt-32 justify-center">
+                <div className="text-center text-3xl text-black font-bold">
+                    Executive Board
+                </div>
+            </div>
+            <div className="h-32 grid grid-rows-1 grid-flow-col gap-4 ml-20">
+                <Bio name="Logan Tullai" position="President" blurbs="Brown‘25, Political Science and Economics"> </Bio>
+                <Bio name="William Deckelbaum" position="Vice President" blurbs="Brown‘24, Mathematical Physics"> </Bio>
+                <Bio name="Michael Kuharski" position="Speaker" blurbs="Brown ‘24, Biochemistry"> </Bio>
+            </div>
+            <div className="h-32"> </div>
+            <div className="h-32 grid grid-rows-1 grid-flow-col gap-4 ml-20">
+                <Bio name="Vacant" position="Secretary" blurbs="Hello World"> </Bio>
+                <Bio name="Vacant" position="Treasurer" blurbs="Hello World"> </Bio>
+                <Bio name="Vacant" position="Director of Membership and Public Relations" blurbs="Hello World"> </Bio>
+            </div>
+            <div className="h-32"> </div>
+        </div>
+    );
+}
+
+export default Events;
