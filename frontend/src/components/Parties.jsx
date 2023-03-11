6172023eec079fc531ec4fb09138f23d920cebe0
Naphat Permpredanun <68365175+NaphatPRM@users.noreply.github.com>
2023-03-11 13:51:22 -0500

    Update Parties.jsx

--- frontend/src/components/Parties.jsx
+++ frontend/src/components/Parties.jsx
@@ -1,8 +1,8 @@
 const Parties = ({ name, image, blurbs }) => {
     return (
-        <div className="h-32 w-1/3 mx-6">
+        <div className="h-32 w-3/5">
             <div className="text-xl text-black font-bold">
-                <img className="object-cover" src={image} alt={"Pic for Events"}/>
+                <img className="object-cover" src={image} alt={"Pic for Parties"}/>
             </div>
             <div className="text-xl text-black font-bold text-center">
                 {name}
