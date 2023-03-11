a88a13354974338b9a30dd2b695efdb55d17b643
Naphat Permpredanun <68365175+NaphatPRM@users.noreply.github.com>
2023-03-11 13:59:39 -0500

    Update Constitution.js

--- frontend/src/Constitution.js
+++ frontend/src/Constitution.js
@@ -1,166 +1,30 @@
-import Navbar from "./components/Navbar";
-import Footnote from "./components/Footnote";
 import './App.css';
+import constitution from "./data/constitution.json";
+import Constitution from "./components/Constitution";
 
 function Constitution() {
     return (
-        <div>
-            <Navbar></Navbar>
-            <div className="h-48 mt-14 justify-center">
-                <div className="text-center text-3xl text-black font-bold">
+        <div className="px-24">
+            <div className="mt-32 justify-center">
+                <h2 className="text-center text-4xl text-black font-bold">
                     Constitution
-                </div>
-                <div className="text-center text-xl text-black">
+                </h2>
+                <h3 className="text-center text-2xl text-black">
                     Brown Political Union
-                </div>
+                </h3>
             </div>
-            <div className="h-48 mt-14 justify-center">
-                <div className="text-xl text-black font-bold">
-                    Article I:  Name of Student Group
-                </div>
-                <div className="text-l text-black">
-                    Section 1: The name of this organization shall be the Brown Political Union. in nextline <br /> <br />
-                    Section 2: This organization may informally be called “the BPU” or “the Union.”
-                </div>
+            <div className="mt-20">
+                {constitution
+                    .map((cont, i) => {
+                        return (
+                            <Constitution
+                                key={i}
+                                head={cont.header}
+                                content={cont.info}
+                            />
+                        );
+                    })}
             </div>
-            <div className="h-48 mt-6 justify-center">
-                <div className="text-xl text-black font-bold">
-                    Article 2: Statement of Purpose:
-
-                </div>
-                <div className="text-l text-black">
-                    Section 1: The purpose of the Brown Political Union shall be to provide students with a multi partisan
-                    forum for political discourse and ideological engagement through debates,
-                    speaker series, discussions, and other events. <br /> <br />
-
-                    Section 2: The Union shall be open to any and all political viewpoints in its capacity as a forum. <br /> <br />
-
-                    Section 3: In pursuit of communal betterment, the Union shall strive to promote political education
-                    and involvement within the Brown community. <br /> <br />
-
-                    Section 4: The Brown Political Union and all of its affiliated events shall be in compliance with
-                    all federal laws and University regulations presently in force and hereafter enacted.
-                </div>
-            </div>
-            <div className="h-48 mt-6 justify-center">
-                <div className="text-xl text-black font-bold">
-                    Article 3:  Party Structure
-
-                </div>
-                <div className="text-l text-black">
-                    Section 1: The Brown Political Union shall maintain a multi-party structure to mediate political
-                    and ideological discourse and facilitate the expression of diverse viewpoints in an ordered setting.
-                    These parties shall evolve and be maintained to reflect the ideological leanings of current Union
-                    members, who shall be responsible for party formation, governance, and dissolution.
-                </div>
-            </div>
-            <div className="h-48 mt-6 justify-center">
-                <div className="text-xl text-black font-bold">
-                    Article 4:  Party Formation and Dissolution
-
-                </div>
-                <div className="text-l text-black">
-                    Section 1: Upon joining the Brown Political Union, members shall be unaffiliated with a Party.
-                    Members may join an existing Party, form a new Party, or remain unaffiliated. <br /> <br />
-
-                    Section 2: Members wishing to form a new Party within the Brown Political Union shall submit a
-                    petition to the Speaker of the Union. The petition shall list the valid number of signatures to
-                    meet the Party Petition Qualifications. <br /> <br />
-
-                    Section 3: The Party Petition Qualifications shall be at least three members of the Brown Political Union. <br /> <br />
-
-                    Section 4: The Speaker of the Brown Political Union shall verify the Party Petition within one week
-                    of receiving it. Following its verification, members who signed the petition will automatically
-                    forfeit any other Party membership and become members of the newly formed Party. The members of
-                    the new Party shall elect a Floor Leader to represent and lead the Party within ten days of the
-                    Party’s verification by the Speaker. <br /> <br />
-
-                    Section 5: Should membership of any Party fall below three members for two consecutive semesters,
-                    then that Party shall receive a notice of impending dissolution from the Executive Board no later
-                    than six weeks before the final day of the second consecutive semester. Should the Party increase
-                    its membership to meet Party Petition Qualifications before the end of the semester, then that
-                    Party shall retain its Party status. Otherwise, the Party shall cease to be recognized as a
-                    Party of the Union and its members shall be unaffiliated with a Party.
-
-                </div>
-            </div>
-            <div className="h-48 mt-6 justify-center">
-                <div className="text-xl text-black font-bold">
-                    Article 5: Governance of Parties
-
-                </div>
-                <div className="text-l text-black">
-                    Section 1: Parties shall maintain the right to determine their own structure, internal procedures,
-                    and leadership so long as none violate the policies put forth by this Constitution or future
-                    amendments to it. The Union Executive Board shall not intervene in the internal governance of
-                    parties except in the instance when the actions of a Party are deemed to be in conflict with the
-                    core principles or goals of the Union as set forth in this Constitution. In such a case,
-                    the Executive Board can, by a unanimous vote only, overrule the actions of party leadership.
-                    Such a vote shall only be held after a meeting of the Union Executive Board and the leadership
-                    of the Party in question, during which every effort shall be made to achieve a resolution amenable
-                    to all.
-                </div>
-            </div>
-            <div className="h-48 mt-6 justify-center">
-                <div className="text-xl text-black font-bold">
-                    Article 6:  Statement of Non-Partisanship
-
-                </div>
-                <div className="text-l text-black">
-                    Section 1: The Brown Political Union is committed to fostering a forum that respects and gives
-                    audience to all perspectives of all members and other discussion participants. With this goal in
-                    mind, participants will be encouraged to constructively challenge and probe the ideas of others
-                    while examining their own preconceptions in such a way that encourages all parties to deepen their
-                    thoughts and understanding of the topic at hand. The Brown Political Union does not and shall not
-                    endorse any particular point of view offered in the course of discussion and is hereby deemed a
-                    neutral entity. Any views expressed by Executive Board officers and other members represent the
-                    individual views of those persons and not the official position of the Brown Political Union,
-                    irrespective of their title with respect to Brown Political Union leadership.
-                </div>
-            </div>
-            <div className="h-48 mt-6 justify-center">
-                <div className="text-xl text-black font-bold">
-                    Article 7: Membership
-
-                </div>
-                <div className="text-l text-black">
-                    Section 1: Eligibility to the Brown Political Union is open to all full-time undergraduate members
-                    of the Brown community and does not discriminate on the basis of race, color, religion, age,
-                    national or ethnic origin, disability, status as a veteran, sexual orientation, gender identity,
-                    gender expression, or sex. Majority membership consists of full-time undergraduate students. <br /> <br />
-
-                    Section 2: Membership in the Union shall be divided into the classes of Full Members and
-                    Honorary Members. <br /> <br />
-
-                    Section 3: Full Membership in the Union shall be granted to any current Brown University student,
-                    faculty, or staff who has attended at least one Union meeting or event and has expressed a desire
-                    for membership status to Union leadership. Full Membership shall expire upon graduation or
-                    termination of student, faculty, or staff status, at which point the individual may be considered
-                    for Honorary Membership. <br /> <br />
-
-                    Section 4: The Executive Board may grant Honorary Membership to any non-member of the Brown
-                    Political Union for any length of time it wishes. Honorary Members shall enjoy the same participatory
-                    privileges as Full Members, but shall not have voting privileges nor the right to hold office.
-                </div>
-            </div>
-            <div className="h-48 mt-6 justify-center">
-                <div className="text-xl text-black font-bold">
-                    Article 8: Leadership and Officers
-
-                </div>
-                <div className="text-l text-black">
-                    Section 1: The Brown Political Union is committed to fostering a forum that respects and gives
-                    audience to all perspectives of all members and other discussion participants. With this goal in
-                    mind, participants will be encouraged to constructively challenge and probe the ideas of others
-                    while examining their own preconceptions in such a way that encourages all parties to deepen their
-                    thoughts and understanding of the topic at hand. The Brown Political Union does not and shall not
-                    endorse any particular point of view offered in the course of discussion and is hereby deemed a
-                    neutral entity. Any views expressed by Executive Board officers and other members represent the
-                    individual views of those persons and not the official position of the Brown Political Union,
-                    irrespective of their title with respect to Brown Political Union leadership.
-                </div>
-            </div>
-            <Footnote></Footnote>
         </div>
     );
 }
