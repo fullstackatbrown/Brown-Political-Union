9d07418ab4004343dda0e8a319e94fac0bcb7ed5
Naphat Permpredanun <68365175+NaphatPRM@users.noreply.github.com>
2023-03-11 13:44:06 -0500

    Update Leadership.js

--- frontend/src/Leadership.js
+++ frontend/src/Leadership.js
@@ -2,26 +2,30 @@ import Navbar from "./components/Navbar";
 import Footnote from "./components/Footnote";
 import './App.css';
 import Bio from "./components/Bio";
+import leadership from "./data/leadership.json";
 
 function Leadership() {
     return (
         <div>
             <Navbar></Navbar>
-            <div className="h-48 mt-14 justify-center">
+            <div className="h-32 mt-24 justify-center">
                 <div className="text-center text-3xl text-black font-bold">
                     Executive Board
                 </div>
             </div>
-            <div className="h-32 grid grid-rows-1 grid-flow-col gap-4 ml-20">
-                <Bio name="Logan Tullai" position="President" blurbs="Brown‘25, Political Science and Economics"> </Bio>
-                <Bio name="William Deckelbaum" position="Vice President" blurbs="Brown‘24, Mathematical Physics"> </Bio>
-                <Bio name="Michael Kuharski" position="Speaker" blurbs="Brown ‘24, Biochemistry"> </Bio>
-            </div>
-            <div className="h-32"> </div>
-            <div className="h-32 grid grid-rows-1 grid-flow-col gap-4 ml-20">
-                <Bio name="Vacant" position="Secretary" blurbs="Hello World"> </Bio>
-                <Bio name="Vacant" position="Treasurer" blurbs="Hello World"> </Bio>
-                <Bio name="Vacant" position="Director of Membership and Public Relations" blurbs="Hello World"> </Bio>
+            <div className="h-screen grid grid-cols-3 gap-x-8 gap-y-24 ml-28">
+                {leadership
+                    .map((leader, i) => {
+                        return (
+                            <Bio
+                                key={i}
+                                name={leader.name}
+                                position={leader.position}
+                                blurbs={leader.blurbs}
+                                image={leader.image}
+                            />
+                        );
+                    })}
             </div>
             <div className="h-32"> </div>
             <Footnote></Footnote>
