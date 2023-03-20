7a3271086e25a619654f8a1b826754913d348862
Naphat Permpredanun <68365175+NaphatPRM@users.noreply.github.com>
2023-03-20 19:58:12 -0400

    Add files via upload

--- frontend/src/components/Footnote.jsx
+++ frontend/src/components/Footnote.jsx
@@ -1,30 +1,57 @@
-const Footnote = () => {
-  return (
-    <section class="flex flex-col lg:flex-row justify-between items-center bg-[#650202] px-5 lg:px-20 py-8">
-      <div>
-        <div class="text-5xl text-white font-bold">BPU</div>
-        <div class="h-5"></div>
-        <div class="text-l text-white">
-          The Brown Political Union is a nonpartisan student-run organization
-        </div>
-        <div class="text-l text-white">
-          dedicated to providing a forum for respectful political.
-        </div>
-        <div class="text-l text-white">discourse on campus.</div>
-      </div>
+import { SocialIcon } from 'react-social-icons';
 
-      <div class="hidden lg:grid grid-rows-2 grid-flow-col gap-10 mt-8 lg:mt-0">
-        <div class="text-xl text-white font-bold inline-block">About Us</div>
-        <div class="text-xl text-white font-bold inline-block">Events</div>
-        <div class="text-xl text-white font-bold inline-block">
-          Constitution
-        </div>
-        <div class="text-xl text-white font-bold inline-block">Parties</div>
-        <div class="text-xl text-white font-bold inline-block">Leadership</div>
-        <div class="text-xl text-white font-bold inline-block">Contact</div>
-      </div>
-    </section>
-  );
-};
+const Footnote = () => {
+    return (
+        <section className="h-72 bg-custom_brown">
+            <div className="float-left inline-block pl-10 pt-12">
+                <div className="text-5xl text-white font-bold">
+                    BPU
+                </div>
+                <div className="h-5">
+
+                </div>
+                <div className="text-l text-white">
+                    The Brown Political Union is a nonpartisan student-run organization
+                </div>
+                <div className="text-l text-white">
+                    dedicated to providing a forum for respectful political.
+                </div>
+                <div className="text-l text-white">
+                    discourse on campus.
+                </div>
+                <div className="mt-6">
+                    <div className="float-left">
+                        <SocialIcon url="https://facebook.com" fgColor="white" />
+                    </div>
+                    <div className="ml-4 float-left">
+                        <SocialIcon url="https://instagram.com" fgColor="white"/>
+                    </div>
+                </div>
+            </div>
+
+            <div className="float-right grid grid-rows-2 grid-flow-col gap-6 pt-12 pr-20">
+                <div className="text-2xl text-white font-bold inline-block">
+                    About Us
+                </div>
+                <div className="text-2xl text-white font-bold inline-block">
+                    Events
+                </div>
+                <div className="text-2xl text-white font-bold inline-block">
+                    Constitution
+                </div>
+                <div className="text-2xl text-white font-bold inline-block">
+                    Parties
+                </div>
+                <div className="text-2xl text-white font-bold inline-block">
+                    Leadership
+                </div>
+                <div className="text-2xl text-white font-bold inline-block">
+                    Contact
+                </div>
+            </div>
+        </section>
+    );
+}
 
 export default Footnote;
+
