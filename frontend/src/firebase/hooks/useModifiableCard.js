6e6264a48934bb43b7859213c6f789b6fee4e672
QuinnStraus <79025802+QuinnStraus@users.noreply.github.com>
2023-11-02 23:14:56 -0400

    Different notification on error

--- frontend/src/firebase/hooks/useModifiableCard.js
+++ frontend/src/firebase/hooks/useModifiableCard.js
@@ -8,18 +8,28 @@ export function useModifiableCard(collection, id, baseData, toastTitle) {
     const [currentData, setCurrentData] = useState(baseData);
 
     const modifyCard = async () => {
-        toast.success(`${toastTitle} modified!`);
         setCurrentImage(currentData.image);
         const firestore = getFirestore()
         const documentRef = doc(firestore, collection, id);
-        await updateDoc(documentRef, currentData);
+        try {
+            await updateDoc(documentRef, currentData);
+            toast.success(`${toastTitle} modified!`);
+        } catch (error) {
+            console.error(error)
+            toast.error(`Error modifying ${toastTitle}`);
+        }
     };
 
     const deleteCard = async () => {
-        toast.success(`${toastTitle} deleted`);
         const firestore = getFirestore()
         const documentRef = doc(firestore, collection, id);
-        await deleteDoc(documentRef);
+        try {
+            await deleteDoc(documentRef);
+            toast.success(`${toastTitle} deleted`);
+        } catch (error) {
+            console.error(error)
+            toast.error(`Error deleting ${toastTitle}`);
+        }
     };
 
     return {
